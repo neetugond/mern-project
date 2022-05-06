1d92cc0972e0634ab5787721859ffc732c41b6e3
neetugond <93507242+neetugond@users.noreply.github.com>
2022-05-06 08:51:12 +0530

    async await

--- server/router/auth.js
+++ server/router/auth.js
@@ -8,24 +8,63 @@ const User = require('../model/userSchema')
 //     res.send("hello from router")
 // })
 
-router.post("/register", (req, res) => {
-     // array destructring to avoid req.body.name 
+// // using promises
+// router.post("/register", (req, res) => {
+//      // array destructring to avoid req.body.name
+//     const { name, email, phone, work, password, cpassword } = req.body
+// // if any of the details missing error show
+//     if (!name || !email || !phone || !work || !password || !cpassword) {
+//         return res.status(422).json({ error: "plz filled the field properly" });
+//     }
+// // if email is already exist error show
+//     User.findOne({ email: email }).then((userExist) => {
+//         if (userExist) {
+//             return res.status(422).json({ error: "Email already exist" })
+//         }
+//         // if not exist create a new user
+//         const user = new User({ name, email, phone, work, password, cpassword });
+// // save the data on atlas
+//         user.save().then(() => {
+//             res.status(201).json({ message: "user registered successfully" });
+//         }).catch((err) => res.status(500).json({ error: "Failed to registered" }));
+//     }).catch(err => {console.log(err)})
+// })
+
+// using async await
+router.post("/register", async (req, res) => {
+    // array destructring to avoid req.body.name 
     const { name, email, phone, work, password, cpassword } = req.body
-// if any of the details missing error show
+    // if any of the details missing error show 
+    // validation
     if (!name || !email || !phone || !work || !password || !cpassword) {
         return res.status(422).json({ error: "plz filled the field properly" });
     }
-// if email is already exist error show
-    User.findOne({ email: email }).then((userExist) => {
+    
+    try {
+        const userExist = await User.findOne({ email: email })
         if (userExist) {
             return res.status(422).json({ error: "Email already exist" })
         }
-        // if not exist create a new user
         const user = new User({ name, email, phone, work, password, cpassword });
-// save the data on atlas
-        user.save().then(() => {
-            res.status(201).json({ message: "user registered successfully" });
-        }).catch((err) => res.status(500).json({ error: "Failed to registered" }));
-    }).catch(err => {console.log(err)})
+
+        // this
+
+        await user.save();
+
+        res.status(201).json({ message: "user registered successfully" });
+
+        // or
+
+        // const userRegister = await user.save();
+        // if (userRegister) {
+        //     res.status(201).json({ message: "user registered successfully" });
+        // } else {
+        //     res.status(500).json({ error: "Failed to registered" })
+        // }
+
+    } catch (err) {
+        console.log(err);
+    }
 })
+
  module.exports = router
