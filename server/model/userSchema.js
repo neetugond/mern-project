eb6ebbb56585688206b44db30ee16ba87ef69555
neetugond <93507242+neetugond@users.noreply.github.com>
2022-05-09 03:22:43 +0530

    jwt sign and store

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 
+const jwt = require('jsonwebtoken')
+
 const bcrypt = require('bcryptjs')
 
 // document structure defined
@@ -27,7 +29,16 @@ const userSchema = new mongoose.Schema({
     cpassword: {
         type: String,
         required: true
-    }
+    },
+    tokens: [
+        // need to generate token again and again that's why array of obj
+        {
+            token: {
+                type: String,
+                required: true
+            }
+        }
+    ]
 })
     
 
@@ -48,11 +59,28 @@ userSchema.pre('save', async function (next) {
 // userSchema.pre('save', async function (next) {
 //     //modified only password
 //      if (this.isModified('email')) {
-//          this.email = await bcrypt.hash(this.email, 12); //12 round 
+//          this.email = await bcrypt.hash(this.email, 12); //12 round
 //      }
 //      next()
 // })
  
+
+// we are generating token
+// userSchema > methods > generateAuthToken
+userSchema.methods.generateAuthToken = async function () {
+    try {
+        // for generating token jwt sign
+        // in sign need to pass two parameter one is payload(obj) and mush be unique we have _id and another is secret key
+        let gentoken = jwt.sign({ _id: this._id }, process.env.SECRET_KEY); //generate token done
+        this.tokens = this.tokens.concat({ token: gentoken }) //for adding concate
+        await this.save(); //need to save
+        return gentoken;
+    
+    } catch (err){
+        console.log(err)
+}
+    
+}
 
 
 // now we need to attach this document with our project we can do that with the help of model = collection create
--- server/router/auth.js
+++ server/router/auth.js
@@ -7,32 +7,6 @@ const router = express.Router();
 
 const User = require('../model/userSchema')
 
-// router.get('/', (req, res) => {
-//     res.send("hello from router")
-// })
-
-// // using promises
-// router.post("/register", (req, res) => {
-//      // array destructring to avoid req.body.name
-//     const { name, email, phone, work, password, cpassword } = req.body
-// // if any of the details missing error show
-//     if (!name || !email || !phone || !work || !password || !cpassword) {
-//         return res.status(422).json({ error: "plz filled the field properly" });
-//     }
-// // if email is already exist error show
-//     User.findOne({ email: email }).then((userExist) => {
-//         if (userExist) {
-//             return res.status(422).json({ error: "Email already exist" })
-//         }
-//         // if not exist create a new user
-//         const user = new User({ name, email, phone, work, password, cpassword });
-// // save the data on atlas
-//         user.save().then(() => {
-//             res.status(201).json({ message: "user registered successfully" });
-//         }).catch((err) => res.status(500).json({ error: "Failed to registered" }));
-//     }).catch(err => {console.log(err)})
-// })
-
 // using async await
 router.post("/register", async (req, res) => {
     // array destructring to avoid req.body.name 
@@ -80,9 +54,14 @@ router.post("/signin", async (req, res) => {
         }
         const userLogin = await User.findOne({ email: email })
         //   console.log('userLogin:', userLogin)
+
         //  compare passwordthrough bcrypt compare
         if (userLogin) {
             const isMatch = await bcrypt.compare(password, userLogin.password)
+
+            // generate jwt token; //why here becoz we are getting all the details in userLogin 
+            const token = await userLogin.generateAuthToken();
+            console.log(token)
         if (!isMatch) {
             res.status(400).json({ error: "user error" });
         } else {
